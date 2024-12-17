2245f7b75c34e8da8348f14130fca267a87f2106
kukumber <arsen.shvedyuk@gmail.com>
2024-12-17 18:40:55 +0200

    1.0.0

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-        {id: '1', title: 'Хуинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+        {id: '1', title: 'Amanda', price: 40, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Julia', price: 40, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Maria', price: 40, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Anna', price: 40, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Julia', price: 40, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 40, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 40, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 40, description: 'Зеленого цвета, теплая'},
 ]
 
 const getTotalPrice = (items = []) => {
