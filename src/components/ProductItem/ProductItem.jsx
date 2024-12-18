61f5959dc1c33354364bb3cbc3680e02a0d2e6f0
kukumber <arsen.shvedyuk@gmail.com>
2024-12-18 04:37:23 +0200

    1.1.2

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "../Button/Button";
 import './ProductItem.css';
 
-const ProductItem = ({product, className, onAdd}) => {
+const ProductItem = ({ product, className, onAdd }) => {
+    const [isInCart, setIsInCart] = useState(false); // Локальное состояние кнопки
 
     const onAddHandler = () => {
-        onAdd(product);
+        setIsInCart(!isInCart); // Переключение состояния кнопки
+        onAdd(product); // Вызов переданного метода "onAdd"
     }
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}/>
+            <div className={'img'} />
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
                 <span>Цена: <b>{product.price}$</b></span>
             </div>
-            <Button className={'add-btn'} onClick={onAddHandler}>
-                Добавить
+            <Button
+                className={`add-btn ${isInCart ? 'in-cart' : ''}`} // Динамическое добавление класса кнопке
+                onClick={onAddHandler}
+            >
+                {isInCart ? 'Удалить' : 'Добавить'} {/* Динамический текст кнопки */}
             </Button>
         </div>
     );
